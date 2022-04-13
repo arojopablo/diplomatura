4f10ee1d49096734f725daa64cefc757ae1391f6
arojopablo <arojopablo@gmail.com>
2022-04-13 09:51:41 -0300

    backend con sistema de login

---
+++ m5u4/backend/models/usuariosModel.js
@@ -0,0 +1,15 @@
+var pool = require('./db');
+var md5 = require('md5');
+console.log("usuariosmodel");
+async function getUserByUserNameAndPassword(user, password) {
+    try {
+        var query = 'SELECT * FROM usuarios WHERE usuario = ? AND password = ? limit 1';
+        var rows = await pool.query(query, [user, md5(password)]);
+        return rows[0];          
+    } catch(error) {
+        console.log("hay error");
+        console.log(error);
+    }
+} 
+
+module.exports = { getUserByUserNameAndPassword }
